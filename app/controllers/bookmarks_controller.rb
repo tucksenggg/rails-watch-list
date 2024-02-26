4f662bc3273d486499ddea210535d2a6edf2b26b
tuckseng <tucksenggg@gmail.com>
2024-02-26 16:02:42 +0800

    updated file

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,31 @@
+class BookmarksController < ApplicationController
+  def new
+    @list = List.find(params[:list_id])
+    @bookmark = Bookmark.new
+    @bookmark.list = @list
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @list = List.find(params[:list_id])
+    @bookmark.list = @list
+    @bookmark.save
+    if @bookmark.save
+      redirect_to list_path(@bookmark.list_id)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark), status: :see_other
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+end
---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,27 @@
+class ListsController < ApplicationController
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+    @bookmarks = Bookmark.includes(:movie).where(list_id: params[:id])
+  end
+
+  def new
+    @new_list = List.new
+  end
+
+  def create
+    @create_list = List.new(list_params)
+    @create_list.save
+    redirect_to lists_path
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+end
